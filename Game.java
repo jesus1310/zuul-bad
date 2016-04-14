134cbc4bd0b9461dd25ba91cc88b1aa3ed7c4ca5
Jesus <jfr1013@gmail.com>
2016-04-14 12:17:58 +0200

    0115: Añadida palabra look a los comandos

--- Game.java
+++ Game.java
@@ -122,6 +122,9 @@ public class Game
         else if (commandWord.equals("quit")) {
             wantToQuit = quit(command);
         }
+        else if (commandWord.equals("look")){
+            printLocationInfo();
+        }
 
         return wantToQuit;
     }
@@ -139,7 +142,7 @@ public class Game
         System.out.println("around at the university.");
         System.out.println();
         System.out.println("Your command words are:");
-        System.out.println("   go quit help");
+        System.out.println("   go quit help look");
     }
 
     /** 
