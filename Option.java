34874881db95b74c3253d163834ca7a5111de327
Jesus <jfr1013@gmail.com>
2016-05-05 09:49:42 +0200

    0124: Añadido atributo String a la clase Option

--- Option.java
+++ Option.java
@@ -6,5 +6,18 @@
  */
 public enum Option
 {
-    GO, QUIT, HELP, LOOK, EAT, BACK, TAKE, DROP, ITEMS, UNKNOWN
+    GO("go"), QUIT("quit"), HELP("help"), LOOK("look"), EAT("eat"), BACK("back"), TAKE("take"), DROP("drop"), ITEMS("items"), UKNOWN("uknown");
+    
+    private String comando;
+    
+    private Option(String comando){
+        this.comando = comando;
+    }
+    
+    /**
+     * Método para devolver el comando
+     */
+    public String getComando(){
+        return comando;
+    }
 }
